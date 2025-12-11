fc3f0c93182fa419d00374ca9a00eed6343beace
mustfamoolan <almoq3m@gmail.com>
2025-12-11 17:17:28 +0300

    Add FIREBASE_CREDENTIALS env variable support in config

--- config/firebase.php
+++ config/firebase.php
@@ -9,7 +9,7 @@ return [
     | Path to the Firebase service account JSON file.
     |
     */
-    'credentials' => storage_path('app/firebase-service-account.json'),
+    'credentials' => env('FIREBASE_CREDENTIALS', storage_path('app/firebase-service-account.json')),
 
     /*
     |--------------------------------------------------------------------------
